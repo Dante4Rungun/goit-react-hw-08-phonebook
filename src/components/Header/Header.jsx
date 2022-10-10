5630739e154de214089b554b08e5735fca16c2cb
Dante4Rungun <denischuprin9619@gmail.com>
2022-10-10 19:36:04 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import styled from "./Header.module.css"
 import { AuthNav } from "components/AuthNav/AuthNav";
 import { useAuth } from "hooks/useAuth";
 import { UserMenu } from "components/UserMenu/UserMenu";
-import { isLoggedIn, selectUser } from "redux/auth/auth-selectors";
+import { selectUser } from "redux/auth/auth-selectors";
 import { useSelector } from "react-redux";
 
 export const Header = () => {
