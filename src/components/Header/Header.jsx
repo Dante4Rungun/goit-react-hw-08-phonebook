5e862fbfbb68e6b323395be07112beee43506c46
Dante4Rungun <denischuprin9619@gmail.com>
2022-10-12 20:20:00 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,6 @@ import styled from "./Header.module.css"
 import { AuthNav } from "components/AuthNav/AuthNav";
 import { useAuth } from "hooks/useAuth";
 import { UserMenu } from "components/UserMenu/UserMenu";
-import { selectUser } from "redux/auth/auth-selectors";
-import { useSelector } from "react-redux";
 
 export const Header = () => {
     const { isLoggedIn } = useAuth()
