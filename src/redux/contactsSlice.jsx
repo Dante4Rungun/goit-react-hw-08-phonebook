48588b7f238c997958f4ead4c092acc41afbafeb
Dante4Rungun <denischuprin9619@gmail.com>
2022-10-14 18:19:10 +0300

    Update contactsSlice.jsx

--- src/redux/contactsSlice.jsx
+++ src/redux/contactsSlice.jsx
@@ -38,7 +38,7 @@ const contactsSlice = createSlice({
       state.isLoading = false
       state.error = null
       console.log(action.payload)
-      const index = state.items.findIndex(contact => contact.id === action.payload.id)
+      const index = state.items.findIndex(contact => contact.id === action.meta.arg)
       state.items.splice(index, 1)
     },
     [deleteContact.rejected]: handleRejecting
